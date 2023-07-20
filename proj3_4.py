abd385752a45c6f00e93ea61c81d0339a5c139c7
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-20 10:23:37 +0530

    Removed some errors

--- proj3_4.py
+++ proj3_4.py
@@ -41,7 +41,7 @@ with open('population-estimates_csv.csv','r') as populationfile:
                          Thailand.append(population['Population'])
                     elif population['Region']=='Viet Nam':
                          Vietnam.append(population['Population'])
-barwidth=0.25
+barwidth=0.06
 fig = plt.subplots(figsize =(12, 8))
 br1=[i for i in range(len(years))]
 br2=[x + barwidth for x in br1]
@@ -53,24 +53,17 @@ br7=[x + barwidth for x in br6]
 br8=[x + barwidth for x in br7]
 br9=[x + barwidth for x in br8]
 br10=[x + barwidth for x in br9]
-#br_list=[br1,br2,br3,br4,br5,br6,br7,br8,br9,br10]
-#asean_list=[Brunei,Cambodia,Indonesia,Laos,Malaysia,Myanmar,Phillippines,Singapore,Thailand,Vietnam]
-""" for country in asean_list:
-     for i in br_list:
-           for region in asean:
-                plt.bar(i,country, width= barwidth, label=region)
-                break
-           break """
+
 plt.bar(br1, Brunei, width= barwidth, color= 'r', label= 'Brunei')
 plt.bar(br2, Cambodia, width= barwidth, color= 'b', label = 'Cambodia')
 plt.bar(br3, Indonesia, width= barwidth, color='g', label= 'Indonesia')
 plt.bar(br4, Laos, width= barwidth, color= 'y', label= " Lao People's Democratic Republic")
-plt.bar(br3, Malaysia, width= barwidth, color='m', label= 'Malaysia')
-plt.bar(br3, Myanmar, width= barwidth, color='c', label= 'Myanmar')
-plt.bar(br3, Phillippines, width= barwidth, color='purple', label= 'Philippines')
-plt.bar(br3, Singapore, width= barwidth, color='pink', label= 'Singapore')
-plt.bar(br3, Thailand, width= barwidth, color='grey', label= 'Thailand')
-plt.bar(br3, Vietnam, width= barwidth, color='brown', label= 'Viet Nam')
+plt.bar(br5, Malaysia, width= barwidth, color='m', label= 'Malaysia')
+plt.bar(br6, Myanmar, width= barwidth, color='c', label= 'Myanmar')
+plt.bar(br7, Phillippines, width= barwidth, color='purple', label= 'Philippines')
+plt.bar(br8, Singapore, width= barwidth, color='pink', label= 'Singapore')
+plt.bar(br9, Thailand, width= barwidth, color='grey', label= 'Thailand')
+plt.bar(br10, Vietnam, width= barwidth, color='brown', label= 'Viet Nam')
 plt.xticks([r + barwidth for r in range(len(years))],years)
 plt.legend()
 plt.show()
