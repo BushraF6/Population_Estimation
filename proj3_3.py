5211369501139a41228f7565633a6c5ec6635dd1
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-22 12:27:17 +0530

    Updated proj3_3.py

--- proj3_3.py
+++ proj3_3.py
@@ -1,36 +1,17 @@
 import csv
 from matplotlib import pyplot as plt
 
-def saarc_population():
-    afg_population, bang_population, bhut_population =0,0,0
-    ind_population, mald_population, nep_population =0,0,0
-    pak_population, sri_population =0,0
-    with open('population-estimates_csv.csv','r') as populationfile:
-        population_reader=csv.DictReader(populationfile)
-        for country in population_reader:
-            if country['Region']=='Afghanistan':
-                afg_population+=round(float(country['Population']))
-            elif country['Region']=='Bangladesh':
-                bang_population+=round(float(country['Population']))
-            elif country['Region']=='Bhutan':
-                bhut_population+=round(float(country['Population']))
-            elif country['Region']=='India':
-                ind_population+=round(float(country['Population']))
-            elif country['Region']=='Maldives':
-                mald_population+=round(float(country['Population']))
-            elif country['Region']=='Nepal':
-                nep_population+=round(float(country['Population']))
-            elif country['Region']=='Pakistan':
-                pak_population+=round(float(country['Population']))
-            elif country['Region']=='Sri Lanka':
-                sri_population+=round(float(country['Population']))
-    saarc_population=[afg_population, bang_population, bhut_population, 
-                        ind_population, mald_population, nep_population, 
-                        pak_population, sri_population]
-    saarc=['Afghanistan', 'Bangladesh', 'Bhutan', 
+saarc=['Afghanistan', 'Bangladesh', 'Bhutan', 
        'India', 'Maldives', 'Nepal', 
        'Pakistan', 'Sri Lanka']
-    plt.bar(saarc,saarc_population)
-    plt.ylim(1000,60000000,10000)
-    plt.show()
-saarc_population()
+saarc_pop={}
+with open('population-estimates_csv.csv','r') as population_file:
+    population_reader=csv.DictReader(population_file)
+    for population in population_reader:
+        if population['Region'] in saarc:
+            if population['Region'] not in saarc_pop:
+                saarc_pop[population['Region']]=0
+            saarc_pop[population['Region']]+=round(float(population['Population']))
+plt.bar(saarc,list(saarc_pop.values()))
+plt.ylim(1000,60000000,10000)
+plt.show()
