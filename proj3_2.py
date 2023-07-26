f7f3a9cadc65121081482319a0e6db8d4a58aff2
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-26 12:06:58 +0530

    Added Comments

--- proj3_2.py
+++ proj3_2.py
@@ -1,16 +1,19 @@
+""" Importing modules to solve the problem."""
 import csv
 from matplotlib import pyplot as plt
 
-def asean_2014():
-    asean=['Brunei Darussalam', 'Cambodia', 'Indonesia', "Lao People's Democratic Republic",
-        'Malaysia', 'Myanmar', 'Philippines',
-        'Singapore', 'Thailand', 'Viet Nam']
-    with open('population-estimates_csv.csv','r') as populationfile:
-            population_reader=csv.DictReader(populationfile)
-            for population in population_reader:
-                if population['Year']=='2014' and population['Region'] in asean:
-                    plt.barh(population['Region'],float(population['Population']))
-    plt.xlabel=('ASEAN Countries')
-    plt.ylabel("Population Count")
-    plt.show()
-asean_2014()     
+# List containing asean country names.
+asean = ['Brunei Darussalam', 'Cambodia',
+         'Indonesia', "Lao People's Democratic Republic",
+         'Malaysia', 'Myanmar', 'Philippines',
+         'Singapore', 'Thailand', 'Viet Nam']
+# Opening file to extract data.
+with open('population-estimates_csv.csv', 'r', encoding='utf-8') as popfile:
+    population_reader = csv.DictReader(popfile)
+    # Calculate the asean countries population in year 2014 and plot it.
+    for population in population_reader:
+        if population['Year'] == '2014' and population['Region'] in asean:
+            plt.barh(population['Region'], float(population['Population']))
+plt.xlabel('ASEAN Countries')
+plt.ylabel('Population Count')
+plt.show()
