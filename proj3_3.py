a54613c46f9023eb112c52894486e060f9ed5a8e
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-26 12:07:55 +0530

    Added Comments

--- proj3_3.py
+++ proj3_3.py
@@ -1,17 +1,22 @@
+""" Importing required modules to solve the problem. """
 import csv
 from matplotlib import pyplot as plt
 
-saarc=['Afghanistan', 'Bangladesh', 'Bhutan', 
-       'India', 'Maldives', 'Nepal', 
-       'Pakistan', 'Sri Lanka']
-saarc_pop={}
-with open('population-estimates_csv.csv','r') as population_file:
-    population_reader=csv.DictReader(population_file)
-    for population in population_reader:
-        if population['Region'] in saarc:
-            if population['Region'] not in saarc_pop:
-                saarc_pop[population['Region']]=0
-            saarc_pop[population['Region']]+=round(float(population['Population']))
-plt.bar(saarc,list(saarc_pop.values()))
-plt.ylim(1000,60000000,10000)
+# Creating list of names of SAARC countries.
+saarc = ['Afghanistan', 'Bangladesh', 'Bhutan',
+         'India', 'Maldives', 'Nepal',
+         'Pakistan', 'Sri Lanka']
+# Creating dictionary to store countries and their population count.
+saarc_pop = {}
+with open('population-estimates_csv.csv', 'r', encoding='utf-8') as popfile:
+    population_reader = csv.DictReader(popfile)
+    for popl in population_reader:
+        if popl['Region'] in saarc:
+            if popl['Region'] not in saarc_pop:
+                saarc_pop[popl['Region']] = 0
+            saarc_pop[popl['Region']] += round(float(popl['Population']))
+plt.bar(saarc, list(saarc_pop.values()))
+plt.ylim(1000, 60000000, 10000)
+plt.xlabel('SAARC Countries')
+plt.ylabel('Population_Count')
 plt.show()
